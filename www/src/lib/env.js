c674f1862a94f8f4bc6b7b0d81e310037b508f67
James <tetrago@allriagon.com>
2024-02-23 18:39:56 -0500

    Add basic website example page

---
+++ www/src/lib/env.js
@@ -0,0 +1,3 @@
+import { MOTMOT_API_ENDPOINT } from '$env/static/public';
+
+export const BASE_API_PATH = `${MOTMOT_API_ENDPOINT}/v1`
---
+++ www/src/routes/groups/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+    import { BASE_API_PATH } from '$lib/env';
+
+    let departmentField = "";
+    let codeField = "";
+
+    /** @type {Promise<{ label: string, name: string }[]>}*/
+    $: groupsPromise = Promise.resolve([]);
+
+    async function departmentChanged() {
+        const res = await fetch(`${BASE_API_PATH}/course/department/${departmentField}`)
+        return await res.json();
+    }
+</script>
+
+<div class="join flex justify-center">
+    <input on:change={() => groupsPromise = departmentChanged()} bind:value={departmentField} type="text" maxlength="3" class="join-item input input-bordered w-full max-w-40" placeholder="COP" />
+    <input type="text" bind:value={codeField} maxlength="5" class="join-item input input-bordered w-full max-w-80" placeholder="3031" />
+</div>
+
+{#await groupsPromise}
+    <progress class="progress w-full"></progress>
+{:then groups}
+    {#if groups.length == 0}
+        <div role="alert" class="mt-5 alert">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-info shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+            <span>It's pretty empty in here...</span>
+        </div>
+    {:else}
+        {#each groups.filter(({ label }) => label.includes(codeField)) as { label, name }, i}
+            <div class="mt-5 card bg-base-200 shadow-xl">
+                <div class="card-body">
+                    <h2 class="card-title">{label}</h2>
+                    <p>{name}</p>
+                </div>
+            </div>
+        {/each}
+    {/if}
+{:catch error}
+    <div role="alert" class="mt-5 alert alert-error">
+        <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+        <span>We can't access our systems at the moment...</span>
+    </div>
+{/await}
