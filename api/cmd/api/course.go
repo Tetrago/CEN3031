c280566755354206d8b11898da31c917e1bcc900
James <tetrago@allriagon.com>
2024-02-23 18:37:53 -0500

    Fix CORS and cache UF API results

--- api/cmd/api/course.go
+++ api/cmd/api/course.go
@@ -22,17 +22,25 @@ type course struct {
 	Name  string `json:"title"`
 }
 
+var cache = make(map[string][]course)
+
 func queryDepartmentCourses(dep string) ([]course, error) {
+	if v, ok := cache[dep]; ok {
+		return v, nil
+	}
+
 	url := fmt.Sprintf("https://catalog.ufl.edu/course-search/api/?page=fose&route=search&subject=%s", dep)
 	request := fmt.Sprintf(`{"other":{"srcdb":""},"criteria":[{"field":"subject","value":"%s"}]}`, dep)
 
 	res, err := http.Post(url, "application/json", bytes.NewBuffer([]byte(request)))
 	if err != nil {
+		cache[dep] = nil
 		return nil, err
 	}
 
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
+		cache[dep] = nil
 		return nil, err
 	}
 
@@ -41,9 +49,11 @@ func queryDepartmentCourses(dep string) ([]course, error) {
 	}
 
 	if err := json.Unmarshal(body, &dest); err != nil {
+		cache[dep] = nil
 		return nil, err
 	}
 
+	cache[dep] = dest.Courses
 	return dest.Courses, nil
 }
 
