5dcc4b6722e1f0d1b89c0ada0d917947decbdb71
James <jamestgeiss@gmail.com>
2024-03-28 14:09:46 -0400

    Tweaks

--- www/src/routes/+page.svelte
+++ www/src/routes/+page.svelte
@@ -12,7 +12,7 @@
 	// Function to fetch popular groups
 	async function fetchPopularGroups() {
 			try {
-					const response = await fetch ('${Base_API_PATH}/group/popular/5');
+					const response = await fetch (`${BASE_API_PATH}/group/popular/5`);
 					if (response.ok) {
 							popularGroups = await response.json();
 					} else {
@@ -30,7 +30,7 @@
 <div class="container mx-auto">
 	<!-- Logo and Name -->
 	<div class="flex justify-center items-center mt-8">
-		<img src={ logo } alt="Logo" class="h-64 w-64 mr-2" />
+		<img src={ logo } alt="Logo" class="h-64 w-64 mr-2 rounded-xl shadow-xl" />
 	</div>
 
 	<div class="flex justify-center items-center mt-8">
@@ -41,15 +41,19 @@
 	<p class="flex justify-center items-center mt-4 text-lg">It's also our all-encompassing chat app that lets you interact with fellow students taking the same UF class as you.</p>
 	<p class="flex justify-center items-center mt-4 text-lg"><i> Ready to get started?</i></p>
 
-	<ul class="mr-1 px-1">
-		<li><a href="/courses" class="flex justify-center items-center rounded-full font-bold text-lg"><i>Click to search and join courses...</i></a></li>
+	<ul class="mr-1 px-1 flex justify-center w-full my-5">
+		<a class="btn btn-accent rounded-full" href="/courses">Search courses</a>
 	</ul>
 
 	<!-- Popular Groups Section -->
-	<h2 class="flex justify-center items-center mt-8 text-2xl font-semibold ml-2">Your most popular groups:</h2>
-	<ul class="mt-4">
-		{#each popularGroups as group}
-				<li>{group.name}</li>
-		{/each}
-	</ul>
+	<div class="card bg-base-200 shadow-xl w-1/2 mx-auto">
+		<div class="card-body">
+			<h2 class="card-title">The most popular groups right now:</h2>
+			<ul class="mt-4">
+				{#each popularGroups as group}
+					<li>{group.name}</li>
+				{/each}
+			</ul>
+		</div>
+	</div>
 </div>
